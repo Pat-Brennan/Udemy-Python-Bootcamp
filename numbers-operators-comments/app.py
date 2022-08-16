4e5a766ba0d81baa2635f2abe65738413cb3dc4b
Patrick Brennan <patbrennan7@gmail.com>
2022-08-16 14:18:24 -0400

    Numbers Continued

--- numbers-operators-comments/app.py
+++ numbers-operators-comments/app.py
@@ -1,10 +1,42 @@
-# Numbers!
+# * Numbers
 
-# Integer(int): A whole number. Takes up less memory than a FLOAT
+# ? Integer(int): A whole number. Takes up less memory than a FLOAT
+
+from __future__ import division
+
 
 print(type(6)) # <class 'int'>
 
-#Floating Point(float): A number with a decimal point
+# ? Floating Point(float): A number with a decimal point
 
 print(type(6.66)) # <class 'float'>
 
+# ? If any part of equation is float, the solution will be float
+
+print(1 + 1.0) # 2.0
+
+# ! However, you CANNOT do this with strings!
+
+print (1 + "2") # this will throw a TYPE ERROR
+
+# * COMMON OPERATORS
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# ** Exponentiation
+# % Modulo
+# // integer Division
+
+# ? In Python, Division will always give you a FLOAT back
+# ? even if they both start as INT's
+
+print(1 / 2) # 0.5
+
+# PEMDAS
+# Parenthesis
+# exponents
+# multiplication
+# division
+# addition
+# subtration
