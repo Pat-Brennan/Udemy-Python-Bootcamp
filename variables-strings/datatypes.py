6f4af262ae1a3094144bf5a9a0b9759660eb6d52
Patrick Brennan <patbrennan7@gmail.com>
2022-08-17 14:58:24 -0400

    Data Types Started

---
+++ variables-strings/datatypes.py
@@ -0,0 +1,7 @@
+
+# * Data Types!
+# ? bool : True or false
+# ? int : an integer (1, 2, 3)
+# ? str : a string. A sequence of unicode characters
+# ? list : an ordered sequence of values of other data types [1, 2, 3]
+# ? dict : A dictionary. A collection of Key:values pairs {"first_name":"Jerry"}
