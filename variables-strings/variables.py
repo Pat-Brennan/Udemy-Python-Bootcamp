13d50721aa5ebd03362af7087e67df5b813c4e48
Patrick Brennan <patbrennan7@gmail.com>
2022-08-17 14:46:40 -0400

    naming coventions covered

---
+++ variables-strings/variables.py
@@ -0,0 +1,41 @@
+
+# * Variables!
+# ?  A variable in python is a named SYMBOL that holds VALUE
+# ? In other words, just a way to STORE DATA
+# ? x = 666
+# ? khaleesi_mother_of_dragons = 1
+
+# * Variable Reassignment
+# ? Variables can be reassigned at anytime!
+
+python_is_awesome = 5
+another_variable = python_is_awesome
+python_is_awesome = 1000
+
+print(another_variable) # prints 5
+
+# * Multiple Variable Assignment
+# ? In python, you can assign multiple variables at once! 
+
+all, at, once = 5, 10, 15 
+
+print(all, at) # prints 5 10
+
+# * Variable Naming Restrictions
+# ? Variables must start with a LETTER or UNDERSCORE
+# ? ✅ _cats ❌ 2cats
+
+# ? Variables are also CASE SENSITIVE
+# ? CATS !== cats
+
+# ? Generally, Variables are written in SNAKE CASE in python NOT CAMEL CASE like JS
+# ? ✅ cats_are_cute ❌ catsAreCute
+
+# ? Constants are written in SCREAMING SNAKE CASE
+# ? SCREAMING_SNAKE_CASE 
+
+# ? Classes are written in UPPER CAMEL CASE (Title case?)
+# ? UpperCamelCase
+
+# ? Variables that are not meant to be touched are written with DUNDERS
+# ? __do_not_touch__
