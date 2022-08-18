f0c1bf551557cd0142d99cdef85e20035e1b9f8c
Patrick Brennan <patbrennan7@gmail.com>
2022-08-18 13:58:33 -0400

    Dynamic v. Static Typing Covered

--- variables-strings/datatypes.py
+++ variables-strings/datatypes.py
@@ -2,6 +2,42 @@
 # * Data Types!
 # ? bool : True or false
 # ? int : an integer (1, 2, 3)
-# ? str : a string. A sequence of unicode characters
+# ? str : a string. A sequence of unicode characters in ""
 # ? list : an ordered sequence of values of other data types [1, 2, 3]
 # ? dict : A dictionary. A collection of Key:values pairs {"first_name":"Jerry"}
+
+
+from operator import not_
+
+
+game_over = False  # Bool
+
+i_am_number = 666  # int
+
+i_am_string = "I'm mr.meeseeks look at me!!!"  # string
+
+i_am_list = [0, "Hey you!", "stop that!", 4, 5, True]  # list
+
+i_am_dictionary = {"Animal": "Froggo", "Food": "Flies",
+                    "Hometown": "Pondville"}  # dictionary
+
+# * Dynamic Typing
+# ? The ability to reassign variables to different types readily
+
+awesomeness = True
+print(awesomeness)
+
+awesomeness = "A meow meow!"
+print(awesomeness)
+
+awesomeness = None  # literally means nothing
+print(awesomeness)
+
+awesomeness = 22 / 7
+print(awesomeness)
+
+# * Static Typing
+# ? The inability to reassign variables to different types(C++ for example)
+
+# int not_awesomeness = 5
+# not_awesomeness = "Great!"  this would throw an error
