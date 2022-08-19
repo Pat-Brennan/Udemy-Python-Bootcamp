67bb2a62bb790751e12e3497eae882005a9f76da
Patrick Brennan <patbrennan7@gmail.com>
2022-08-19 15:35:43 -0400

    Input Dialogue Created

---
+++ boolean-and-conditional-logic/input.py
@@ -0,0 +1,21 @@
+
+# * Input!
+# ? A built-in function in python that will prompt the user and
+# ? the store the result to a variable.
+print('WOAH! Sorry...Dont see too many folks out here. Whats your name?')
+name = input()
+
+print(f'Well nice to meet you { name } !')
+
+print('What brings you into my neck of the woods?')
+reason = input()
+
+print(f'{ reason }?!?!?! I cant believe this...')
+
+print('Do the others know?')
+check = input()
+
+print(f'{ check } ? Good, good...')
+
+print('Well you be carful now!')
+goodbye = input('(exit)')
