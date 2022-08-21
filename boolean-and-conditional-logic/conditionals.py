41e84e9914054edb6376d53bb7d616844b5010d7
Patrick Brennan <patbrennan7@gmail.com>
2022-08-21 15:56:42 -0400

    Multiple Elif Statement Covered

--- boolean-and-conditional-logic/conditionals.py
+++ boolean-and-conditional-logic/conditionals.py
@@ -21,3 +21,19 @@ else:
     print('Well... Carry on then!')
 
 # ! INDENTATION MATTERS! IT'S HOW PYTHON TELLS WHAT'S RELATED TO WHAT!
+
+# * Multiple elif Example
+
+print('What\'s your favorite color?') # Use of escape characters
+color = input()
+
+if color == 'Black' or 'black': # Use of OR boolean Operator
+  print('That would be correct!')
+elif color == 'Red' or 'red':
+  print('That would also be correct!')
+elif color == 'Turquoise' or 'turquoise':
+  print('What are you some kind of Turtle??')
+elif color == 'Yellow' or 'yellow':
+  print('Are you aware that can make babies cry?')
+else:
+  print('I can\'t believe what I am hearing!')
