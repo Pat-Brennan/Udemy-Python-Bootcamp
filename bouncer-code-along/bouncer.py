6368de3d6b71d0c540f9b1394cd3d35d0f873207
Patrick Brennan <patbrennan7@gmail.com>
2022-08-25 15:44:45 -0400

    Bouncer has been hired!

--- bouncer-code-along/bouncer.py
+++ bouncer-code-along/bouncer.py
@@ -2,25 +2,29 @@
 # * Bouncer code along!
 
 # ask for age
-
-from re import I
-
-
 print('What is your age Noble Concert Patron?')
 age = input()
 
-if age == '':
-  print("I'll ask you one more time friend,")
-  print("What is your age?")
-  age = input()
-elif int(age) < 21 and int(age) >= 18:
-    print("No drinking for you friend! A band of caution will be issued!")
-elif age >= 21:
-    print("Enjoy the show friend! Drink up!")
+if age != '':
+    age = int(age)
+    if age < 21 and age >= 18:
+      print("No drinking for you friend! A band of caution will be issued!")
+    elif age >= 21:
+        print("Enjoy the show friend! Drink up!")
+    else:
+        print("Thou has not spent enough time on this earth for such entertainment!")
 else:
-    print("Thou has not spent enough time on this earth for such entertainment!")
+    print("I'll ask you one more time friend,")
+    print("What is your age?")
 
-
-# 18 - 21 wristband
-# 21+ to drink, normal entry
-# too young, sorry no entry
+# ? Refactored
+# if age:
+#     age = int(age)
+#     if age >= 21:
+#         print("Enjoy the show friend! Drink up!")
+#     elif age >= 18:
+#         print("No drinking for you friend! A band of caution will be issued!")
+#     else:
+#         print("Thou has not spent enough time on this earth for such entertainment!")
+# else:
+#     print("Please enter an Age!")
