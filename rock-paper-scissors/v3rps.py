5a3e107809af8e7d6943f864dc25382901423810
Patrick Brennan <patbrennan7@gmail.com>
2022-09-06 11:53:33 -0400

    Computer Player Added

---
+++ rock-paper-scissors/v3rps.py
@@ -0,0 +1,40 @@
+import random
+
+# * Rock, Paper, Scissors!
+
+print('... Rock ...')
+print('... Paper ...')
+print('... Scissors ...')
+
+print('Player:')
+player = input().lower()
+
+print('Computer:')
+comp = random.randint(0, 2)
+if comp == 0:
+    comp = "rock"
+elif comp == 1:
+    comp = "paper"
+elif comp == 2:
+    comp = "scissors"
+print(comp)
+
+if player == comp:
+    print("DRAWWW!")
+elif player == "rock":
+    if comp == "paper":
+        print("Comp Wins!")
+    elif comp == "scissors":
+        print("Player Wins!")
+elif player == "paper":
+    if comp == "scissors":
+        print("Computer Wins!")
+    elif comp == "rock":
+        print("Player Wins!")
+elif player == "scissors":
+    if comp == "rock":
+        print("Computer Wins!")
+    elif comp == "paper":
+        print("Player Wins!")
+else:
+    print("Something seems to be amiss...")
