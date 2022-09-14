f9772f8328d2a5ccfb972a277b3678988cab9aa1
Patrick Brennan <patbrennan7@gmail.com>
2022-09-14 13:28:21 -0400

    Repeater Function Created

---
+++ exercises/screaming-repeating/repeat.py
@@ -0,0 +1,10 @@
+
+# * Repeating exercise! 
+
+# ? Create a loop that will say something over and over again
+
+print('How many times do I need to tell you!?')
+times = int(input())
+
+for time in range(times):
+  print('Clean up your room!!!')
