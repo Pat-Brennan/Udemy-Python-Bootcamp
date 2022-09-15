23fe56a2955d06879c1f38a85fc87a1de862484a
Patrick Brennan <patbrennan7@gmail.com>
2022-09-15 12:49:15 -0400

    Copy Game Created

---
+++ loops/copygame.py
@@ -0,0 +1,15 @@
+
+# * Copy Game! Is it a game? Who knows!
+
+# Make a game that takes input
+# Then repeats that input back to the user
+# until the user uses a SPECIFIC input
+# Then the game ends
+
+print("Hey how's it going?")
+user = input()
+
+while user != "stop copying me":
+  print(user)
+  user = input()
+print("FINE ILL STOP")
