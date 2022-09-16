7feb8ba987e18350b67f7a08ba133fe97b7f33f3
Patrick Brennan <patbrennan7@gmail.com>
2022-09-16 13:56:03 -0400

    Loop added to play multiple times

--- rock-paper-scissors/v3rps.py
+++ rock-paper-scissors/v3rps.py
@@ -1,40 +1,40 @@
 import random
 
 # * Rock, Paper, Scissors!
+for time in range(3):
+    print('... Rock ...')
+    print('... Paper ...')
+    print('... Scissors ...')
 
-print('... Rock ...')
-print('... Paper ...')
-print('... Scissors ...')
+    print('Player:')
+    player = input().lower()
 
-print('Player:')
-player = input().lower()
+    print('Computer:')
+    comp = random.randint(0, 2)
+    if comp == 0:
+        comp = "rock"
+    elif comp == 1:
+        comp = "paper"
+    elif comp == 2:
+        comp = "scissors"
+    print(comp)
 
-print('Computer:')
-comp = random.randint(0, 2)
-if comp == 0:
-    comp = "rock"
-elif comp == 1:
-    comp = "paper"
-elif comp == 2:
-    comp = "scissors"
-print(comp)
-
-if player == comp:
-    print("DRAWWW!")
-elif player == "rock":
-    if comp == "paper":
-        print("Computer Wins!")
-    elif comp == "scissors":
-        print("Player Wins!")
-elif player == "paper":
-    if comp == "scissors":
-        print("Computer Wins!")
-    elif comp == "rock":
-        print("Player Wins!")
-elif player == "scissors":
-    if comp == "rock":
-        print("Computer Wins!")
-    elif comp == "paper":
-        print("Player Wins!")
-else:
-    print("Something seems to be amiss...")
+    if player == comp:
+        print("DRAWWW!")
+    elif player == "rock":
+        if comp == "paper":
+            print("Computer Wins!")
+        elif comp == "scissors":
+            print("Player Wins!")
+    elif player == "paper":
+        if comp == "scissors":
+            print("Computer Wins!")
+        elif comp == "rock":
+            print("Player Wins!")
+    elif player == "scissors":
+        if comp == "rock":
+            print("Computer Wins!")
+        elif comp == "paper":
+            print("Player Wins!")
+    else:
+        print("Something seems to be amiss...")
