46f6716fe19d681c3831f5a44ca1da43afc8382b
Patrick Brennan <patbrennan7@gmail.com>
2022-09-22 17:08:23 -0400

    Donation Problem Solved

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -66,3 +66,12 @@ for attributes in cat.keys():
 
 for k, v in cat.items():
     print(f'The key is {k}, and the value is {v}')
+
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5,
+                  stan=150.0, lisa=50.25, harrison=10.0)
+print(donations)
+
+total_donations = 0
+for donation in donations.values():
+    total_donations += donation
+    print(total_donations)
