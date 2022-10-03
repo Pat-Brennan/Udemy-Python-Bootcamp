87b57526fb28063cf5f482c7f0e7ea270e09d545
Patrick Brennan <patbrennan7@gmail.com>
2022-10-03 16:27:55 -0400

    kwargs Covered

--- functions/V2functions.py
+++ functions/V2functions.py
@@ -33,3 +33,16 @@ def ensure_correct_info (*creds): # Again, DOES NOT NEED TO BE ARGS
 print(ensure_correct_info("Pat", "Brennan"))
 print(ensure_correct_info("Kevin", "Seagull"))
 
+
+# * '** Kwargs'
+# ? Uses Two stars, and is pronounced 'QWARGS'
+# ? The 'kw' stands for 'Key Word'
+# ? A Special operator that we can pass to functions
+# ? Gathers remaining KEYWORD ARGUMENTS as a DICTIIONARY
+# ! This is just a parameter - you can call it whatever you want! 
+
+def fav_colors (**kwargs):
+  for person, color in kwargs.items():
+    print(f"{person}'s favorite color is {color}")
+
+fav_colors(taylor='Blue', jonah='purple', kevin='green')
