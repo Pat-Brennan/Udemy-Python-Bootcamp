28ca5915c2e19ad04cb82c389ef8de6acbc5d132
Patrick Brennan <patbrennan7@gmail.com>
2022-10-03 15:16:05 -0400

    * Args Started

---
+++ functions/V2functions.py
@@ -0,0 +1,8 @@
+
+# * Even more Functions!
+
+# * '* args'
+# ? A special operator we can pass to functions
+# ? Gather remaining arguments as a tuple.
+# ! This is just a parameter! - YOU CAN CALL IT WHATEVER YOU WANT!
+
