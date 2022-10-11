6cae7165f88ab34b2427ad156aaa33777854b8a9
Patrick Brennan <patbrennan7@gmail.com>
2022-10-11 15:41:52 -0400

    Modules Started

---
+++ modules/modules.py
@@ -0,0 +1,7 @@
+
+# * Modules!
+
+# * Why use modules? 
+# ? Keep Python files small
+# ? reuse code across multiple files by IMPORTING
+# ? A Module is just a Python file!
---
+++ modules/rand_module.py
@@ -0,0 +1,8 @@
+
+# * Random Module!
+
+import random
+
+print(random.choice(['apple', 'oranges', 'basketball', 'meteor']))
+print(random.shuffle(['apple', 'oranges', 'basketball', 'meteor']))
+print(random.randint(1, 100))
