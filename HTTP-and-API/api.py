72cfc457712630f42eedfd75f2f4b199a7255978
Patrick Brennan <patbrennan7@gmail.com>
2022-10-13 15:51:24 -0400

    API Defined

---
+++ HTTP-and-API/api.py
@@ -0,0 +1,11 @@
+
+# * API!
+# ? Application Programming Interface
+# ? Allows you to get data from another application without needing
+# ? to understand how the application works.
+# ? Can often send data back in different formats.
+# ? Examples of companies with APIs: Github, Spotify, Google
+
+# * API Formats:
+# ? JSON : JavaScript Object Notation
+# ? XML :
--- HTTP-and-API/http.py
+++ HTTP-and-API/http.py
@@ -2,26 +2,22 @@
 # * HTTP: Hyper Text Transfer Protocol
 
 # * What happens when you enter a URL into the Browser?
-
 # ? 1. DNS(Domain name system) lookup 
 # ? 2. Computer makes a REQUEST to a server
 # ? 3. Server processes the REQUEST
 # ? 4. Server issuesss a RESPONSE
 
 # * So what is a DNS Lookup?
-
 # ? Like a phonebook but for the internet!
 # ? The Process: google.com -> DNS Server -> 172.213.9.142
 # ? In other words: The destination -> the phonebook -> the (IP) address 
 # ? IP = Internet Protocol
 
 # * The request - response cycle
-
 # ? The process: client -> HTTP request -> server -> (sometimes database) -> reponse
 # ? In other words: Your computer -> talks to server -> server looks for information -> Sends info back
 
 # * HTTP Headers
-
 # ? Sent with both requests and responses
 # ? They provide additional information about the request or response
 
@@ -30,3 +26,18 @@
 # ? 300's - Redirect
 # ? 400's - Client Error (You did this!)
 # ? 500's - Server Error (Hey! It's not your fault.)
+
+# * HTTP Verbs
+# ? Similar terms, Request method = Request verb
+
+# ? GET: Useful for retrieving data
+# ?      Data passed in query string
+# ?      Should have no "side-effects"
+# ?      Can be cached
+# ?      Can be bookmarked
+
+# ? POST: Useful for writing data
+# ?       Data passed in request body
+# ?       Can have "side-effects"
+# ?       Not cached
+# ?       Can't be bookmarked
