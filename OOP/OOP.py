2c609f97723c34bab1cf188bdd23936fd4699d9f
Patrick Brennan <patbrennan7@gmail.com>
2022-10-19 15:29:49 -0400

    OOP Started

---
+++ OOP/OOP.py
@@ -0,0 +1,14 @@
+
+# * OOP! : Object Oriented Programming
+# ! OOP is NOT unique to Python!
+# ? OOP is  a method of programming that attempts to model some process or thing ->
+# ? in the world as a CLASS or OBJECT.
+
+# * What is a CLASS?
+# ? A blueprint for objects. Classes can contain methods (functions) ->
+# ? and attributes(similar to keys in a dict)
+
+# * What is an INSTANCE?
+# ? Objects that are constructed from a class blueprint that contains their class's ->
+# ? methods and properties.
+
