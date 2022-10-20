0754827fa26ed958ef1537a587c33e509108e1ae
Patrick Brennan <patbrennan7@gmail.com>
2022-10-20 09:55:30 -0400

    First Class Created

--- OOP/OOP.py
+++ OOP/OOP.py
@@ -16,3 +16,25 @@
 # ? With object oriented programming, the goal is to encapsulate your code into ->
 # ? LOGICAL, HIERARCHICAL groupings using classes so that you can reason ->
 # ? about your code at a higher level.
+
+# * Encapsulation
+# ? The grouping of public and private attributes and methods into a ->
+# ? programmatic class, making abstraction possible.
+
+# * Abstraction
+# ? Exposing only "relevant" data in a class interface, hiding private attributes and methods ->
+# ? (aka the "inner workings") from users.
+
+# * Creating a Class
+# ? conventionally named with singular terms, i.e "User", "Dog", "Person"
+# ? also typically written with camelcase instead of snake case
+
+class User:  # this is what every user will look like
+    pass
+
+
+user1 = User()  # This is INSTANTIATING user!
+user2 = User()  # This is INSTANTIATING user!
+user3 = User()  # This is INSTANTIATING user!
+user4 = User()  # This is INSTANTIATING user!
+print(type(user1))
