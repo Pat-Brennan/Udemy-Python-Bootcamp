9aa436b4594f405159b1b4340f003d33c2043e89
Patrick Brennan <patbrennan7@gmail.com>
2022-10-24 15:04:43 -0400

    Code Clean Up

--- OOP/pet.py
+++ OOP/pet.py
@@ -30,27 +30,30 @@ gator = Pet("Big Chonker", "Gator")
 # ? Class methods are methods (with the @classmethod decorator) that are not concerned ->
 # ? with instances, but the class itself
 
+
 class User:  # this is what every user will look like
 
     active_users = 0  # This is a CLASS ATTRIBUTE
 
+
 @classmethod
-def display_active_users(cls): # class is automatically passed in, hence "cls"
+def display_active_users(cls):  # class is automatically passed in, hence "cls"
     return f"There are currently {cls.active_users} active users"
+
 
 @classmethod
 def from_string(cls, data_str):
-    first,last,pet = data_str.split(",")
-    return cls(first,last,pet)
-    
+    first, last, pet = data_str.split(",")
+    return cls(first, last, pet)
+
     # Does not need to be SELF, but it's practical!
     def __init__(self, first, last, pet):
         self.first = first  # These are individual INSTANCES!
         self.last = last
         self.pet = pet
         User.active_users += 1  # This refers to the CLASS ATTRIBUTE
-    
-    def __repr__(self): # repr meaning "Representation"
+
+    def __repr__(self):  # repr meaning "Representation"
         return f"{self.first} has a {self.pet}"
 
     def logout(self):
